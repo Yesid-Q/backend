7eee2b5bc979bf35b1f8cb6915361e9ff950d3a1
Yesid Gonzalez <oquintero@openmytab.com>
2022-08-28 19:38:33 -0500

    cors

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ app: FastAPI = FastAPI(
 
 app.add_middleware( 
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173", "http://0.0.0.0:5173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
